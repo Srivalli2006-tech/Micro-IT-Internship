9a260c7c737e9431a645ec5a7be03933c92f4b67
Srivalli2006-tech <nayanalasrivalli@gmail.com>
2025-05-14 11:58:15 +0530

    Update Password Generator .py

--- Password Generator .py
+++ Password Generator .py
@@ -1,3 +1,6 @@
+"""Basically the user will forget the password if they give some complex or random values"""
+"""so here we are generating the password by user details
+(their name, birth_year, special characters and again the name is repeated from reverse order)"""
 def generate_password(name, birth_year,  special_char="!", length=12):
     base = name[0].upper() + name[1:3].lower() + birth_year[-2:] + special_char + name[::-1].lower()
     # Assemble the password
